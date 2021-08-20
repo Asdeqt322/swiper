ac1a83e05cf9d37b1106143681a8059965a82d80
Asdeqt322 <64643536+Asdeqt322@users.noreply.github.com>
2021-08-20 12:02:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    
+    loop: true, // ,tcrjytxyfz ghjrhenrf
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+   
+  });
